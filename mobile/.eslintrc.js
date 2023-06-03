57649080fa300d0b269fdc7c9c72ce7c63e7829f
Szymon Kozioł <contact@critteros.dev>
2023-06-03 09:59:35 +0200

    Setup TS, prettier and eslint

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+        "@typescript-eslint/consistent-type-imports": "error",
+        "react/react-in-jsx-scope" :"off"
+    }
+}
